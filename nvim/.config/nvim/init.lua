bc637e6ffbbabfa880cfe80c98814a0dc40a8d16
Scazard <eliassamuelsson321@gmail.com>
2025-03-05 22:02:01 +0100

    better modularity of nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,15 +20,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
---vim.keymap.set('n', '<hmm>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-vim.g.mapleader = " "
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
