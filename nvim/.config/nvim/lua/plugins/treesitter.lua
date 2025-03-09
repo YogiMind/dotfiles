922dca6418a49cf05d4ef9fe506aad6d32c2cd40
Scazard <eliassamuelsson321@gmail.com>
2025-03-09 13:11:14 +0100

    none-ls added

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,14 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "html",
-          "json",
-          "lua",
-          "python",
-          "yaml",
-          "cpp",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
