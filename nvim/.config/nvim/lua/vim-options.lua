6087033e595d9b02b212c69ae3afb314579a0577
Scazard <eliassamuelsson321@gmail.com>
2025-03-20 09:47:04 +0100

    Easy clipboard yank and paste

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -8,6 +8,22 @@ vim.cmd("set hidden")
 
 vim.g.mapleader = " "
 
+-- Normal mode: Make <leader>y work just like y but copy to clipboard
+vim.keymap.set('n', '<leader>y', '"+y', { silent = true })
+vim.keymap.set('n', '<leader>Y', '"+Y', { silent = true })
+
+-- Visual mode: Make <leader>y work just like y but copy to clipboard
+vim.keymap.set('v', '<leader>y', '"+y', { silent = true })
+
+-- Normal mode: Paste from clipboard
+vim.keymap.set('n', '<leader>p', '"+p', { silent = true })
+vim.keymap.set('n', '<leader>P', '"+P', { silent = true })
+
+-- Visual mode: Paste from clipboard
+vim.keymap.set('v', '<leader>p', '"+p', { silent = true })
+vim.keymap.set('v', '<leader>P', '"+P', { silent = true })
+
+
 vim.diagnostic.config({
   float = { border = "rounded" }
 })
